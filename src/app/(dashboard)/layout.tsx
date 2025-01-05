8941cbc6e66f8b3dacdae249ed25df3d3c3d9051
PV-duwesh <duwesh.kumar@PVADVISORY.IN>
2025-01-05 20:10:10 +0530

    fix: layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -11,13 +11,15 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+      <div className="w-[14%] md:w-[10%] lg:w-[18%] xl:w-[14%] p-4">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
         >
-          <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block font-bold">Kumar School</span>
+          <Image src="/logo.png" alt="logo" width={30} height={30} />
+          <span className="hidden lg:block font-bold text-sm">
+            Kumar School
+          </span>
         </Link>
         <Menu />
       </div>
